761947149a53d657182bb25cf2c21e08148c4889
Masud Rana <masudrana799dev@gmail.com>
2023-02-17 21:47:44 +0600

    config database

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,41 @@
 
 // external imports
 const express = require('express');
+const morgan = require('morgan');
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+mongoose.set('strictQuery', false);
+
+
+// Load env vars
+// dotenv.config({ path: './config/config.env' });
+// dotenv.config()
+dotenv.config({ path: './backend/.env' });
+
+// Internal imports
+const connectDB = require('./config/db');
+
+
+
+
+// Connect to database
+connectDB();
+
+
+// Route files
+
+
+
 
 const app = express();
+
+// Body parser
+app.use(express.json());
+
+// Dev logging middleware
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 
 const port = process.env.PORT || 5000;
 
